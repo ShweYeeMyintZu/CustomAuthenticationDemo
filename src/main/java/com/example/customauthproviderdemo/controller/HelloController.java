d405eab986f0dae8cdd655b548bd41ff64473aef
shweyee <shweyeemyintzutheinzan@gmail.com>
2023-03-30 22:01:05 +0630

    custom authentication

---
+++ src/main/java/com/example/customauthproviderdemo/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.customauthproviderdemo.controller;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
+}
